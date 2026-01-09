a0618bd0f9347240493522a53e550df6273ffe30
Iloveumom <jtndrbhatia93@gmail.com>
2026-01-09 15:39:52 +0530

    Question - Unknown route

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+// GET /products
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
+});
+
+// POST /products
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
+});
+
+// GET /categories
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
+});
+
+// POST /categories
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
+});
+
+// Wildcard route for undefined paths (404)
+app.use((req, res) => {
+  res.status(404)
+  res.send("<h1>404 - Page Not Found</h1>");
+});
+
+
+// Start server
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
